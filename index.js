86c374a9ccfbd21ab68c9c2e99ed0702f37d062b
Savaliya Dax Kalpeshbhai <daxsavaliya00@gmail.com>
2025-12-29 10:30:29 +0530

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-for(var i=0;i<document.querySelectorAll("button").length;i++){
-    document.querySelectorAll("button")[i].addEventListener("click",function(){
-        alert("I got clicked!");
-    });
-}
- 
