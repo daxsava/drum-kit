aec73589dff5d9207256dc0bb57580b58b50ba60
Savaliya Dax Kalpeshbhai <daxsavaliya00@gmail.com>
2025-12-29 09:20:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+for(var i=0;i<document.querySelectorAll("button").length;i++){
+    document.querySelectorAll("button")[i].addEventListener("click",function(){
+        alert("I got clicked!");
+    });
+}
+ 
